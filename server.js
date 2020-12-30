684bc5db6a25f2d2f9588943e2bf61a42a7f1246
Santosh Rai <me.santosrai@gmail.com>
2020-12-30 01:36:02 +0000

    refac: apply rate limiter to req

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const app = express()
 const port = process.env.PORT || 3001
 const dbUrl = process.env.MONGODB_URI
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
